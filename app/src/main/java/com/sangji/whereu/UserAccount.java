bae4b1b7d4c3c38d1e6d8280f5d7f0292eddd6c3
roswkzm <roswkzm@naver.com>
2021-07-11 18:47:20 +0900

    회원가입 및 로그인 구축

---
+++ app/src/main/java/com/sangji/whereu/UserAccount.java
@@ -0,0 +1,35 @@
+package com.sangji.whereu;
+
+// 사용자 계정 정보 모델 클레스
+public class UserAccount {
+
+    private String idToken;     // Firebase Uid (고유 토큰정보)
+    private String emailId;     // 이메일 아이디
+    private String password;    // 비밀번호
+
+    public UserAccount() { }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
