62aad601925f60b239a367808716af5e8fecc578
roswkzm <roswkzm@naver.com>
2021-07-14 21:31:38 +0900

    서로 체팅 주고받기 기능 구현

--- app/src/main/java/com/sangji/whereu/chat/MessageActivity.java
+++ app/src/main/java/com/sangji/whereu/chat/MessageActivity.java
@@ -2,11 +2,16 @@ package com.sangji.whereu.chat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +22,9 @@ import com.google.firebase.database.ValueEventListener;
 import com.sangji.whereu.ChatModel;
 import com.sangji.whereu.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageActivity extends AppCompatActivity {
 
     private String destinationUid;
@@ -25,6 +33,8 @@ public class MessageActivity extends AppCompatActivity {
 
     private String uid;
     private String chatRoomUid;
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,7 @@ public class MessageActivity extends AppCompatActivity {
         button =(Button)findViewById(R.id.messageActivity_button);
         editText = findViewById(R.id.messageActivity_editText);
 
+        recyclerView = (RecyclerView)findViewById(R.id.messageActivity_recyclerview);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,6 +86,8 @@ public class MessageActivity extends AppCompatActivity {
                     if(chatModel.users.containsKey(destinationUid)){
                         chatRoomUid = item.getKey();
                         button.setEnabled(true);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(MessageActivity.this));
+                        recyclerView.setAdapter(new RecyclerViewAdapter());
                     }
                 }
 
@@ -85,5 +98,57 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
+    class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+        List<ChatModel.Comment> comments;
+        public RecyclerViewAdapter() {
+            comments = new ArrayList<>();
+
+            FirebaseDatabase.getInstance().getReference().child("whereu").child("chatrooms").child(chatRoomUid).child("comments").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    comments.clear();
+                    for(DataSnapshot item : dataSnapshot.getChildren()){
+                        comments.add(item.getValue(ChatModel.Comment.class));
+                    }
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message,parent,false);
+
+            return new MessageViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            ((MessageViewHolder)holder).textView_message.setText(comments.get(position).message);
+        }
+
+        @Override
+        public int getItemCount() {
+            return comments.size();
+        }
+
+        private class MessageViewHolder extends RecyclerView.ViewHolder {
+            public TextView textView_message;
+
+            public MessageViewHolder(View view) {
+                super(view);
+                textView_message = view.findViewById(R.id.messageItem_textView_message);
+            }
+        }
     }
 }
