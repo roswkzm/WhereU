63d647cdd0fec45e1da367979984b648847bb795
woojaeHEO <vlvk69@naver.com>
2021-07-16 00:59:05 +0900

    googlemap

--- app/src/main/java/com/sangji/whereu/MapMainActivity.java
+++ app/src/main/java/com/sangji/whereu/MapMainActivity.java
@@ -1,14 +1,46 @@
 package com.sangji.whereu;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class MapMainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapMainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_main);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this::onMapReady);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.370467, 127.928695); //상지대학교 마커
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("상지대학교");
+        markerOptions.snippet("대학교");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,16));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,16)); //애니메이션 카메라 꾸밈
     }
 }
