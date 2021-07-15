6b04f948ac2a3933d4800819ebfbe1755b01ad58
woojaeHEO <vlvk69@naver.com>
2021-07-16 01:59:31 +0900

    splash start 3s

---
+++ app/src/main/java/com/sangji/whereu/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.sangji.whereu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends  Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super .onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
+    }
+}
