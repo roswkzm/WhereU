40d71b0f2a874ca915c4b99bf1c8b27dc2b17b5e
woojaeHEO <vlvk69@naver.com>
2021-10-08 15:30:22 +0900

    위도/경도 버튼(텍스트뷰) 추출 , Manifest permission

--- app/src/main/java/com/sangji/whereu/MapMainActivity.java
+++ app/src/main/java/com/sangji/whereu/MapMainActivity.java
@@ -13,31 +13,57 @@ import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapMainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private FragmentManager fragmentManager;
-    private MapFragment mapFragment;
-
+    private FusedLocationProviderClient fusedLocationClient;    // 현재 위치를 가져오기 위한 변수
+    int REQUEST_CODE = 1;
+    TextView location_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_main);
 
-        fragmentManager = getFragmentManager();
-        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        //FusedLocationProviderClient의 인스턴스를 생성.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // 위도 경도를 보여줄 뷰를 inflate.
+        location_view = findViewById(R.id.location_view);
+
+        // 위치를 가져오기 위한 버튼.
+        Button btn_get_location;
+        btn_get_location = findViewById(R.id.btn_get_location);
+        btn_get_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // GPS 권한 요청.
+                ActivityCompat.requestPermissions(MapMainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            }
+        });
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
         mapFragment.getMapAsync(this::onMapReady);
     }
+
+
 
     @SuppressLint("MissingPermission")
     @Override
@@ -78,21 +104,35 @@ public class MapMainActivity extends AppCompatActivity implements OnMapReadyCall
         }
     }
 
-    @SuppressLint("MissingPermission")
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        GoogleMap googleMap = null;
-                        googleMap.setMyLocationEnabled(true);
-                    }
-                } else {
-                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {   // 권한요청이 허용된 경우
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show();    // 권한요청이 거절된 경우
+                    return;
                 }
-                return;
+
+                // 위도와 경도를 가져온다.
+                @SuppressLint("MissingPermission") Task<Location> locationTask = fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    // 파라미터로 받은 location을 통해 위도, 경도 정보를 텍스트뷰에 set.
+                                    location_view.setText("위도: " + location.getLatitude() + " / 경도: " + location.getLongitude());
+                                }
+                            }
+                        });
+            }
+            else{
+                Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show();    // 권한요청이 거절된 경우
             }
         }
     }
