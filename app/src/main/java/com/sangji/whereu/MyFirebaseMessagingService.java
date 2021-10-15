cfb8449272b6f86ac29b10a69ddfdf8a3e56ded1
roswkzm <roswkzm@naver.com>
2021-10-16 03:52:10 +0900

    포그라운드 알람 수정

--- app/src/main/java/com/sangji/whereu/MyFirebaseMessagingService.java
+++ app/src/main/java/com/sangji/whereu/MyFirebaseMessagingService.java
@@ -39,6 +39,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this)
